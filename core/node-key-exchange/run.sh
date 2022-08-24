15119a19bef30758430b8d8de06c9d6537d4bb7e
isanghyeon <cpd4268@naver.com>
2022-08-24 12:00:27 +0900

    Updated

---
+++ core/node-key-exchange/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+source ./setting.sh
+
+ENV="key"
+TIMESTAMP=$(date +%Y.%m)
+PATH="../../build/$ENV"
+
+echo "Environment :: " "$ENV"
+echo "Path        :: " "$PATH"
+echo "Build       :: " "$TIMESTAMP-$ENV"
+
+sleep 3
+
+docker build -t cpd9957/named-data-networking:"$TIMESTAMP"-$ENV $PATH
+
+sleep 1
+
+echo "Docker image build completed..."
+
+sleep 3
+
+apt-get update; apt-get install -y docker-compose
+
+docker-compose stop; docker-compose down; docker-compose rm -f; docker network node_network -f; docker-compose build; docker-compose up -d; docker volume node_volume -f; docker-compose logs -f >> $ENV.log& date
+
