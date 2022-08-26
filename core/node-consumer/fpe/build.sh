99be729edca3559ff5324855b74c11fb4ea49a0c
isanghyeon <cpd4268@naver.com>
2022-08-26 23:18:04 +0900

    Update; For Docker build
    2022.08.26 - isanghyeon

---
+++ core/node-consumer/fpe/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./.env
+
+echo "[*] Environment :: " "$ENV"
+echo "[*] Path        :: " "$Path"
+echo "[*] Build       :: " "$TIMESTAMP-$ENV"
+
+sleep 3
+
+docker-compose stop
+docker-compose down
+docker-compose rm -f
+
+docker rmi $Image:$TIMESTAMP-$ENV
+
+docker build -t $Image:$TIMESTAMP-$ENV $Path
+
+if [ $? -eq 1 ]; then
+  echo "[-] Docker image build failed..."
+else
+  echo "[+] Docker image build completed..."
+fi
+
+sleep 3
+
+echo "[*] Done building..."
