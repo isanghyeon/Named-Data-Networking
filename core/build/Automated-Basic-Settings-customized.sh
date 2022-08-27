42020b1aa96df93eca6a4b2537276f618ce1a7d0
isanghyeon <cpd4268@naver.com>
2022-08-27 22:33:09 +0900

    Updated; Automated shell

--- core/build/Automated-Basic-Settings-customized.sh
+++ core/build/Automated-Basic-Settings-customized.sh
@@ -33,16 +33,17 @@ make altinstall
 
 echo Done...
 python3.9 --version
+python3.9 -m pip --version
 
 
 echo "============== Docker Environment ================="
 
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
 stable"
-sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo systemctl enable docker && service docker start
+apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+systemctl enable docker && service docker start
 service docker status
