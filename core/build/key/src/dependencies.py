85dc5ce319f98abc49eba3624335a9bece2a003c
isanghyeon <cpd4268@naver.com>
2022-09-30 16:37:00 +0900

    Delete core/build/key directory

--- core/build/key/src/dependencies.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Header, HTTPException
-
-
-async def getHeaderAuthorization(Authorization: str = Header()):
-    if Authorization is None or not Authorization:
-        raise HTTPException(status_code=400, detail="Bad Request")
-
-
