fb16f5e5b14dfd47f457b9c36aa195287632f503
isanghyeon <cpd4268@naver.com>
2022-11-07 06:26:53 +0900

    Updated; none

--- core/build/none/src/producer.cpp
+++ core/build/none/src/producer.cpp
@@ -19,6 +19,11 @@
  * See AUTHORS.md for complete list of ndn-cxx authors and contributors.
  */
 
+//
+// Created by 이상현 on 2022/11/07.
+//
+
+
 #include <ndn-cxx/face.hpp>
 #include <ndn-cxx/security/key-chain.hpp>
 #include <ndn-cxx/security/signing-helpers.hpp>
@@ -28,92 +33,92 @@
 // Enclosing code in ndn simplifies coding (can also use `using namespace ndn`)
 namespace ndn {
 // Additional nested namespaces should be used to prevent/limit name conflicts
-namespace examples {
+    namespace examples {
 
-class Producer
-{
-public:
-  void
-  run()
-  {
-    m_face.setInterestFilter("/example/testApp/randomData",
-                             std::bind(&Producer::onInterest, this, _1, _2),
-                             nullptr, // RegisterPrefixSuccessCallback is optional
-                             std::bind(&Producer::onRegisterFailed, this, _1, _2));
+        class Producer
+        {
+        public:
+            void
+            run()
+            {
+                m_face.setInterestFilter("/sch.ac.kr/calab/research.file",
+                                         std::bind(&Producer::onInterest, this, _1, _2),
+                                         nullptr, // RegisterPrefixSuccessCallback is optional
+                                         std::bind(&Producer::onRegisterFailed, this, _1, _2));
 
-    auto cert = m_keyChain.getPib().getDefaultIdentity().getDefaultKey().getDefaultCertificate();
-    m_certServeHandle = m_face.setInterestFilter(security::extractIdentityFromCertName(cert.getName()),
-                             [this, cert] (auto&&...) {
-                               m_face.put(cert);
-                             },
-                             std::bind(&Producer::onRegisterFailed, this, _1, _2));
-    m_face.processEvents();
-  }
+                auto cert = m_keyChain.getPib().getDefaultIdentity().getDefaultKey().getDefaultCertificate();
+                m_certServeHandle = m_face.setInterestFilter(security::extractIdentityFromCertName(cert.getName()),
+                                                             [this, cert] (auto&&...) {
+                                                                 m_face.put(cert);
+                                                             },
+                                                             std::bind(&Producer::onRegisterFailed, this, _1, _2));
+                m_face.processEvents();
+            }
 
-private:
-  void
-  onInterest(const InterestFilter&, const Interest& interest)
-  {
-    std::cout << ">> I: " << interest << std::endl;
+        private:
+            void
+            onInterest(const InterestFilter&, const Interest& interest)
+            {
+                std::cout << ">> I: " << interest << std::endl;
 
-    static const std::string content("Hello, world!");
+                static const std::string content("Hello, world!");
 
-    // Create Data packet
-    auto data = make_shared<Data>(interest.getName());
-    data->setFreshnessPeriod(10_s);
-    data->setContent(make_span(reinterpret_cast<const uint8_t*>(content.data()), content.size()));
+                // Create Data packet
+                auto data = make_shared<Data>(interest.getName());
+                data->setFreshnessPeriod(10_s);
+                data->setContent(make_span(reinterpret_cast<const uint8_t*>(content.data()), content.size()));
 
-    // in order for the consumer application to be able to validate the packet, you need to setup
-    // the following keys:
-    // 1. Generate example trust anchor
-    //
-    //         ndnsec key-gen /example
-    //         ndnsec cert-dump -i /example > example-trust-anchor.cert
-    //
-    // 2. Create a key for the producer and sign it with the example trust anchor
-    //
-    //         ndnsec key-gen /example/testApp
-    //         ndnsec sign-req /example/testApp | ndnsec cert-gen -s /example -i example | ndnsec cert-install -
+                // in order for the consumer application to be able to validate the packet, you need to setup
+                // the following keys:
+                // 1. Generate example trust anchor
+                //
+                //         ndnsec key-gen /example
+                //         ndnsec cert-dump -i /example > example-trust-anchor.cert
+                //
+                // 2. Create a key for the producer and sign it with the example trust anchor
+                //
+                //         ndnsec key-gen /example/testApp
+                //         ndnsec sign-req /example/testApp | ndnsec cert-gen -s /example -i example | ndnsec cert-install -
 
-    // Sign Data packet with default identity
-    m_keyChain.sign(*data);
-    // m_keyChain.sign(*data, signingByIdentity(<identityName>));
-    // m_keyChain.sign(*data, signingByKey(<keyName>));
-    // m_keyChain.sign(*data, signingByCertificate(<certName>));
-    // m_keyChain.sign(*data, signingWithSha256());
+                // Sign Data packet with default identity
+                m_keyChain.sign(*data);
+                // m_keyChain.sign(*data, signingByIdentity(<identityName>));
+                // m_keyChain.sign(*data, signingByKey(<keyName>));
+                // m_keyChain.sign(*data, signingByCertificate(<certName>));
+                // m_keyChain.sign(*data, signingWithSha256());
 
-    // Return Data packet to the requester
-    std::cout << "<< D: " << *data << std::endl;
-    m_face.put(*data);
-  }
+                // Return Data packet to the requester
+                std::cout << "<< D: " << *data << std::endl;
+                m_face.put(*data);
+            }
 
-  void
-  onRegisterFailed(const Name& prefix, const std::string& reason)
-  {
-    std::cerr << "ERROR: Failed to register prefix '" << prefix
-              << "' with the local forwarder (" << reason << ")" << std::endl;
-    m_face.shutdown();
-  }
+            void
+            onRegisterFailed(const Name& prefix, const std::string& reason)
+            {
+                std::cerr << "ERROR: Failed to register prefix '" << prefix
+                          << "' with the local forwarder (" << reason << ")" << std::endl;
+                m_face.shutdown();
+            }
 
-private:
-  Face m_face;
-  KeyChain m_keyChain;
-  ScopedRegisteredPrefixHandle m_certServeHandle;
-};
+        private:
+            Face m_face;
+            KeyChain m_keyChain;
+            ScopedRegisteredPrefixHandle m_certServeHandle;
+        };
 
-} // namespace examples
+    } // namespace examples
 } // namespace ndn
 
 int
 main(int argc, char** argv)
 {
-  try {
-    ndn::examples::Producer producer;
-    producer.run();
-    return 0;
-  }
-  catch (const std::exception& e) {
-    std::cerr << "ERROR: " << e.what() << std::endl;
-    return 1;
-  }
+    try {
+        ndn::examples::Producer producer;
+        producer.run();
+        return 0;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        return 1;
+    }
 }
