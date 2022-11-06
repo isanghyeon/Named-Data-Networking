78bb0dda79db0662c376a7e3267478b94a226acb
isanghyeon <cpd4268@naver.com>
2022-11-07 02:58:09 +0900

    Updated; fpe

--- core/build/fpe/src/fpe-consumer.cpp
+++ core/build/fpe/src/fpe-consumer.cpp
@@ -19,27 +19,36 @@
  * See AUTHORS.md for complete list of ndn-cxx authors and contributors.
  */
 
+//
+// Created by 이상현 on 2022/11/07.
+//
+
+
 #include <ndn-cxx/face.hpp>
 #include <ndn-cxx/security/validator-config.hpp>
+#include <ndn-cxx/util/time.hpp>
 
 #include <iostream>
+#include <fstream>
 
 // Enclosing code in ndn simplifies coding (can also use `using namespace ndn`)
 namespace ndn {
 // Additional nested namespaces should be used to prevent/limit name conflicts
     namespace examples {
 
-        class Consumer {
+        class Consumer
+        {
         public:
-            Consumer() {
+            Consumer()
+            {
                 m_validator.load("trust-schema.conf");
             }
 
             void
-            run() {
-                Name first("/localhost/nfd/faces/query/%96%19r%17tcp4%3A%2F%2F172.31.0.1%3A20304");
-
-                Name interestName("/localhost/nfd/faces/query/%96%19r%17tcp4%3A%2F%2F172.30.0.1%3A20304");
+            run()
+            {
+                // Name interestName("/example/testApp/randomData");
+                Name interestName("/sch.ac.kr/calab/research.file");
                 interestName.appendVersion();
 
                 Interest interest(interestName);
@@ -48,7 +57,7 @@ namespace ndn {
 
                 std::cout << "Sending Interest " << interest << std::endl;
                 m_face.expressInterest(interest,
-                                       std::bind(&Consumer::onData, this, _1, _2),
+                                       std::bind(&Consumer::onData, this,  _1, _2),
                                        std::bind(&Consumer::onNack, this, _1, _2),
                                        std::bind(&Consumer::onTimeout, this, _1));
 
@@ -58,25 +67,32 @@ namespace ndn {
 
         private:
             void
-            onData(const Interest &, const Data &data) {
-                std::cout << "Received Data " << data << std::endl;
+            onData(const Interest&, const Data& data)
+            {
+                rtt = time::steady_clock::now() - now;
+                // fp << rtt << endl;
 
-                m_validator.validate(data,
-                                     [](const Data &) {
-                                         std::cout << "Data conforms to trust schema" << std::endl;
-                                     },
-                                     [](const Data &, const security::ValidationError &error) {
-                                         std::cout << "Error authenticating data: " << error << std::endl;
-                                     });
+                std::cout << "Received Data " << data << std::endl;
+                std::cout << "Round-Trip Time " << rtt << std::endl;
+
+//                m_validator.validate(data,
+//                                     [] (const Data&) {
+//                                         std::cout << "Data conforms to trust schema" << std::endl;
+//                                     },
+//                                     [] (const Data&, const security::ValidationError& error) {
+//                                         std::cout << "Error authenticating data: " << error << std::endl;
+//                                     });
             }
 
             void
-            onNack(const Interest &, const lp::Nack &nack) const {
+            onNack(const Interest&, const lp::Nack& nack) const
+            {
                 std::cout << "Received Nack with reason " << nack.getReason() << std::endl;
             }
 
             void
-            onTimeout(const Interest &interest) const {
+            onTimeout(const Interest& interest) const
+            {
                 std::cout << "Timeout for " << interest << std::endl;
             }
 
@@ -89,13 +105,14 @@ namespace ndn {
 } // namespace ndn
 
 int
-main(int argc, char **argv) {
+main(int argc, char** argv)
+{
     try {
         ndn::examples::Consumer consumer;
         consumer.run();
         return 0;
     }
-    catch (const std::exception &e) {
+    catch (const std::exception& e) {
         std::cerr << "ERROR: " << e.what() << std::endl;
         return 1;
     }
