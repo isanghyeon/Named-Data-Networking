edc787bee2e43758546f26c00fd4927a7b737830
isanghyeon <cpd4268@naver.com>
2022-11-07 04:48:09 +0900

    Updated; shellscript

--- core/python.env.sh
+++ core/python.env.sh
@@ -8,13 +8,12 @@ echo "============== Packages Update ================="
 apt-get update
 
 echo "============== Python v3.9.12 Environment ================="
-
-echo Dependency package install
-echo ==============1==============
+echo "Dependency package install"
+echo "==============1=============="
 apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
 
-echo Python v3.9.12 install
-echo ==============2==============
+echo "Python v3.9.12 install"
+echo "==============2=============="
 cd /opt
 pwd
 wget https://www.python.org/ftp/python/3.9.12/Python-3.9.12.tgz
@@ -22,12 +21,12 @@ tar -xf Python-3.9.12.tgz
 cd Python-3.9.12
 ./configure --enable-optimizations
 
-echo Python v3.9.12 build process start
-echo ==============3==============
+echo "Python v3.9.12 build process start"
+echo "==============3=============="
 make -j 12
 
-echo Python v3.9.12 install
-echo ==============4=============
+echo "Python v3.9.12 install"
+echo "==============4============="
 make altinstall
 
 echo Done...
