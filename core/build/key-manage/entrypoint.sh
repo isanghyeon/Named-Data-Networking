49fe001df439b90c3a9a0d60816a11bea6680288
isanghyeon <cpd4268@naver.com>
2022-11-11 17:14:12 +0900

    Updated; key manage node

--- core/build/key-manage/entrypoint.sh
+++ core/build/key-manage/entrypoint.sh
@@ -6,4 +6,4 @@
 
 
 echo "============== REST API (FastAPI) Start ================="
-cd /usr/src/app/src; python3.10 -m uvicorn main:app --host 0.0.0.0 --port 20100 --reload
+cd /usr/src/app/src; python3 -m uvicorn main:app --host 0.0.0.0 --port 20100 --reload
